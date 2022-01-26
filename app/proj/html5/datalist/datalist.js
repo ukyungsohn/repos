d432e20e39a116848ab2d4be98df7593868a7cb4
ukyungsohn <ukson@stclab.com>
2022-01-27 08:57:03 +0900

    .

--- app/proj/html5/datalist/datalist.js
+++ app/proj/html5/datalist/datalist.js
@@ -10,19 +10,53 @@ window.onload = function() {
 
     const input = document.getElementById('search');
     const clickButton = document.getElementById('clickButton');
-    const savingText = document.getElementById('savingText');
+    const textList = document.getElementById('textList');
 
-    input.addEventListener('keydown', function(e) {
-        // console.log(e);        
+    input.addEventListener('keyup', function(e) {
+        //console.log(e.target.value);        
         newArr = [];
+        textList.innerHTML = '';
+        textList.className = 'listBorder';
         for (let i = 0; i < arr.length; ++i) {
-            if (arr[i].includes(e.key)) {
+            //if (arr[i].includes(e.key)) {
+            if (arr[i].includes(e.target.value)) {
                 newArr.push(arr[i]);
+                const div = document.createElement('div');
+                const span = document.createElement('span');
+                span.innerText = arr[i];
+                div.addEventListener('click', function(e) {
+                    input.value = e.target.textContent;
+                    textList.style.display = 'none';
+                });
+                div.append(span);
+                textList.append(div);                
             }
-        }
+        }        
         console.log(newArr);
     });
     
+    input.addEventListener('focus', function(e) {
+        newArr = [];
+        textList.innerHTML = '';
+        textList.className = 'listBorder';
+        for (let i = 0; i < arr.length; ++i) {
+            if (arr[i].includes(e.target.value)) {
+                newArr.push(arr[i]);
+                const div = document.createElement('div');
+                const span = document.createElement('span');
+                span.innerText = arr[i];
+                div.addEventListener('click', function(e) {
+                    input.value = e.target.textContent;
+                    textList.style.display = 'none';
+                });
+                div.append(span);
+                textList.append(div);
+            }
+        }
+        
+
+    });
+
     clickButton.onclick = function() {
         if (arr.indexOf(input.value) < 0) {
             arr.push(input.value);
@@ -30,7 +64,7 @@ window.onload = function() {
         console.log(arr);
         // const span = document.createElement('span');
         // span.innerText = input.value;
-        // savingText.append(span);
+        // textList.append(span);
 
     };
     

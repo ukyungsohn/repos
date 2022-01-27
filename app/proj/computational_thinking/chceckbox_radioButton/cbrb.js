9ef46777e62851405869ae20eb1cb65f0c274e13
Ukyung <right7son@stclab.com>
2022-01-27 16:45:28 +0900

    cbrb 작성중

--- app/proj/computational_thinking/chceckbox_radioButton/cbrb.js
+++ app/proj/computational_thinking/chceckbox_radioButton/cbrb.js
@@ -1,3 +1,18 @@
+function clickCheckbox(e) {
+    e.target.checked = !e.target.checked;
+    if (e.target.checked) {
+        e.target.classList.add('checked');
+        e.target.childNodes[0].classList.add('checkedBox');
+        console.log(e.target.getAttribute('val'));
+    } else {
+        e.target.classList.remove('checked');
+        e.target.childNodes[0].classList.remove('checkedBox');
+    }
+}
+
+function clickButton() {
+
+}
 /*
 let obj = {
     title: '',

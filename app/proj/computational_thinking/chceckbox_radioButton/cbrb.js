88c486f67fe36af6956e4bc98f9622826e6f52e9
ukyungsohn <ukson@stclab.com>
2022-01-27 15:32:35 +0900

    체크박스 라디오버튼 예제 시작

---
+++ app/proj/computational_thinking/chceckbox_radioButton/cbrb.js
@@ -0,0 +1,25 @@
+/*
+let obj = {
+    title: '',
+    selected: {}
+    items: [
+        {},
+        {}
+    ],
+    clickFunc: function() {
+
+    }
+}
+
+let obj2 = {
+    title: '',
+    items: [
+        {checked: true},
+        {checked: false}
+    ],
+    clickFunc: function() {
+
+    }
+}
+
+*/

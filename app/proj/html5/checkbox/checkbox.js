322c87e5bcdc810f7d2e508e85efd1dacdfcdbd9
ukspvtlaptop <life2scool@naver.com>
2022-01-28 19:35:52 +0900

    .

--- app/proj/html5/checkbox/checkbox.js
+++ app/proj/html5/checkbox/checkbox.js
@@ -1,16 +1,45 @@
 function clickCheckbox(e) {
+    console.log(e);
+    e.stopPropagation();
     e.target.checked = !e.target.checked;
     if (e.target.checked) {
-        e.target.classList.add('checked');
+        e.target.classList.add('checked');//toggle
         e.target.childNodes[0].classList.add('checkedBox');
-        console.log(e.target.getAttribute('val'));
     } else {
-        e.target.classList.remove('checked');
+        e.target.classList.remove('checked');//toggle
         e.target.childNodes[0].classList.remove('checkedBox');
     }
 }
 
 function clickButton() {
+    //div를 검색한다.
+    const divs = document.getElementsByClassName('checked');
+    //name속성이 있는 것들에서 name값을 뽑아 새 배열에 넣는다.
+    let names = [];
+    for (let i = 0; i < divs.length; ++i) {
+        console.log(divs[i].getAttribute('name'));
+        if (names.indexOf(divs[i].getAttribute('name')) < 0) {
+            names.push(divs[i].getAttribute('name'));
+        }
+    }   
+    //div chekced된 것 중에 name이 같은 것 끼리 val을 뽑는다.
+    /*
+    { 
+        fruit: [orange, apple],
+        color: [green]
+    }
+    */
+    console.log(names);
+    let obj = {};
+    for (let i = 0; i < names.length; ++i) {
+        obj[names[i]] = [];
+        for (let j = 0; j < divs.length; ++j) {
+            if (divs[j].getAttribute('name') === names[i]) {
+                obj[names[i]].push(divs[j].getAttribute('val'));
+            } 
+        }
+    }
+    console.log(obj);
     
 }
 /*
---
+++ app/proj/html5/radioButton/radioButton.js
@@ -0,0 +1,66 @@
+window.onload = function() {
+    let data = {
+        title: 'Fruit',
+        selected: null,
+        items: [
+            {label: 'Banana', name: 'fruit', value: 'banana', checked: true},
+            {label: 'Apple', name: 'fruit', value: 'apple'}
+        ],
+        click : function(item, ev) {
+            if (item.parent.selected.domObj) {
+                item.parent.selected.domObj.classList.remove('checked');
+            }
+            this.classList.add('checked');
+            item.parent.selected = item;            
+            item.parent.selected.domObj = this;
+            //checked 처리: 
+            //버튼에 클래스 넣어주고
+            //selected에 넣어주고 
+        }
+    };
+
+    makeRadioButton('radioContainer', data);
+
+    function makeRadioButton(id, data) {
+        //id는 받아서 append할거고
+        let container = document.getElementById(id);        
+
+        let initSelectedDiv = null;
+        let initSelectedData = null;
+
+        //data로 dom만들고 클릭 이벤트 넣어야겠지.
+        for (let i = 0; i < data.items.length; ++i) {
+            let div = document.createElement('div');
+            div.setAttribute('name', data.items[i].name);
+            div.setAttribute('value', data.items[i].value);        
+            div.classList.add('radioButton');
+            if (data.items[i].checked) {
+                // div.classList.add('checked');
+                initSelectedDiv = div;
+                initSelectedData = data.items[i];
+            }
+
+            let btnDiv = document.createElement('div');
+            btnDiv.classList.add('button');
+            div.append(btnDiv);
+
+            let circleDiv = document.createElement('div');
+            //circleDiv.classList.add('circle');
+            btnDiv.append(circleDiv);            
+
+            let span = document.createElement('span');
+            span.innerText = data.items[i].label;            
+            div.append(span);
+      
+            data.items[i].parent = data;
+            div.addEventListener('click', data.click.bind(div, data.items[i]));
+            
+            container.append(div);
+        }
+
+        //참조하고 있기 때문에 for문 밖에서 가능.
+        initSelectedDiv.classList.add('checked');
+        data.selected = initSelectedData;
+        data.selected.domObj = initSelectedDiv;
+    }
+};
